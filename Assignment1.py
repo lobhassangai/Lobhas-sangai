b53efd750a4d05d8e73dc22a83eb0cd8d9a2f26c
lobhassangai <64913838+lobhassangai@users.noreply.github.com>
2020-05-09 18:58:53 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+list1 = [12,-7,5,64,-14]
+for num in list1:
+     if num >= 0: 
+       print(num, end = " ") 
+  
+
+
+# In[4]:
+
+
+list2 = [12,14,-95,3]
+for num in list2:
+     if num >= 0: 
+       print(num, end = " ") 
+  
+
+
+# In[ ]:
+
+
+
+
